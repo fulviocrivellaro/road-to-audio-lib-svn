4642f9f9d241351166c20c737264cf21d237b5a4
buccia85@gmail.com <buccia85@gmail.com@cb09e575-b7ae-7bc2-e5b0-a3c7ce2821ad>
2012-09-09 18:47:43 +0000

    Break-through graphical improvements

--- trunk/JuceTutorialBase/Source/MainGUI.h
+++ trunk/JuceTutorialBase/Source/MainGUI.h
@@ -3,7 +3,7 @@
 
   This is an automatically generated file created by the Jucer!
 
-  Creation date:  9 Sep 2012 7:23:05pm
+  Creation date:  9 Sep 2012 8:41:53pm
 
   Be careful when adding custom code to these files, as only the code within
   the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
@@ -19,8 +19,8 @@
   ==============================================================================
 */
 
-#ifndef __JUCER_HEADER_MAINGUI_MAINGUI_5FC5C78__
-#define __JUCER_HEADER_MAINGUI_MAINGUI_5FC5C78__
+#ifndef __JUCER_HEADER_MAINGUI_MAINGUI_CF0395CE__
+#define __JUCER_HEADER_MAINGUI_MAINGUI_CF0395CE__
 
 //[Headers]     -- You can add your own extra header files here --
 #include <../JuceLibraryCode/JuceHeader.h>
@@ -111,4 +111,4 @@ private:
 };
 
 
-#endif   // __JUCER_HEADER_MAINGUI_MAINGUI_5FC5C78__
+#endif   // __JUCER_HEADER_MAINGUI_MAINGUI_CF0395CE__
