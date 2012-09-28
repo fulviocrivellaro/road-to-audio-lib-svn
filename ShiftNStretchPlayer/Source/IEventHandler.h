171d1fb08960fb90fd4d5a66e009b0add8b0eb8d
buccia85@gmail.com <buccia85@gmail.com@cb09e575-b7ae-7bc2-e5b0-a3c7ce2821ad>
2012-09-28 07:13:40 +0000

    Added starting interfaces

---
+++ ShiftNStretchPlayer/Source/IEventHandler.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    IEventHandler
+    Created: 28 Sep 2012 8:49:19am
+    Author:  audiolib
+
+  ==============================================================================
+*/
+
+#ifndef __IEVENTHANDLER_7ABA43EB__
+#define __IEVENTHANDLER_7ABA43EB__
+
+
+
+
+
+#endif  // __IEVENTHANDLER_7ABA43EB__
